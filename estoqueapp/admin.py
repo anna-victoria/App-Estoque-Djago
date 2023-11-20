a8f5de46a72bfcd2a98a3bb0a1c3aa7a390a91f7
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-11-19 22:49:12 -0300

    filtro tabela estoque

--- estoqueapp/admin.py
+++ estoqueapp/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from .models import Estoque
+from .forms import EstoqueFormCreate
 
 # Register your models here.
 
-from .models import Estoque
+class EstoqueAdminCreate(admin.ModelAdmin):
+    list_display = ['categoria', 'nome_item', 'quantidade', 'quantidade_emitida', 'emitido_por', 'emitido_para', 'numero_telefone']
+    form = EstoqueFormCreate
+    
+    # filtrar_lista = ['categoria']
+    # campo_pesquisa = ['categoria', 'nome_item', 'quantidade', 'quantidade_emitida', 'emitido_por', 'emitido_para', 'numero_telefone']
+    # Additional customization options can be added here
 
-admin.site.register(Estoque)
+admin.site.register(Estoque, EstoqueAdminCreate)
