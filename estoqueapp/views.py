1884a83167a8840ba53b8d1ca8fb2c582c6000a8
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-11-22 23:18:54 -0300

    criando a view home

--- estoqueapp/views.py
+++ estoqueapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+	title = 'Página Inicial, bem vindo(a)!'
+	context = {
+	"title": title,
+	}
+	return render(request, "home.html",context)
