6b5003576a147e529f0cad71b09ea4192efc47a0
Anna Victoria Feliciana <anna.feliciana@upe.br>
2024-01-20 21:33:03 -0300

    ajuste na url

--- estoqueapp/urls.py
+++ estoqueapp/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from estoqueapp import views
 from .views import authView, home
 
@@ -10,4 +12,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', authView, name='authView'),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.STATIC_URL)
