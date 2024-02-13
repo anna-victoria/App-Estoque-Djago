0ced18fe9815292479c6a0778c6a97c75c5dd309
Anna Victoria Feliciana <anna.feliciana@upe.br>
2024-02-13 15:11:04 -0300

    mudando para tabela a listagem

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -118,13 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = ['bootstrap5']
+
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
